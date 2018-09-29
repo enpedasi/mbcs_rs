fae5d3e9f4ee6f357d14c11362cafb77ad96083a
Ueno Yoshihiro <enpedasi@gmail.com>
2018-09-29 12:26:43 +0900

    [Modified] Some Encoding expression.

--- test/mbcs_rs_test.exs
+++ test/mbcs_rs_test.exs
@@ -14,12 +14,12 @@ defmodule MbcsRsTest do
     assert MbcsRs.encode!("あいうえお壱弐参四五①②③④⑤", "sjis") |> MbcsRs.decode!("SJIS") ==
              "あいうえお壱弐参四五①②③④⑤"
 
-    assert MbcsRs.encode!("あいうえお", "euc-jp") |> MbcsRs.decode!("euc-jp") == "あいうえお"
+    assert MbcsRs.encode!("あいうえお", "euc-jp") |> MbcsRs.decode!("EUC-JP") == "あいうえお"
 
-    assert MbcsRs.encode!("한국어", "euc-kr") |> MbcsRs.decode!("euc-kr") == "한국어"
+    assert MbcsRs.encode!("한국어", "euc-kr") |> MbcsRs.decode!("EUC-KR") == "한국어"
 
-    assert MbcsRs.encode!("你好，世界", "big5") |> MbcsRs.decode!("big5") == "你好，世界"
+    assert MbcsRs.encode!("你好，世界", "big5") |> MbcsRs.decode!("BIG5") == "你好，世界"
 
-    assert MbcsRs.encode!("信息交换用汉字编码字符集", "GB2312") |> MbcsRs.decode!("GB2312") == "信息交换用汉字编码字符集"
+    assert MbcsRs.encode!("信息交换用汉字编码字符集", "gb2312") |> MbcsRs.decode!("GB2312") == "信息交换用汉字编码字符集"
   end
 end
